76687a9c6ed0a768f3e4f52ae3579cb4e4aa9315
Svitlana <svitlanagayduchyk76@gmail.com>
2024-07-18 12:19:03 +0300

    urls for groups

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class GroupMemberInLine(admin.TabularInline):
+    model = models.Member
+
+
+admin.site.register(models.Group)
+
+
--- groups/urls.py
+++ groups/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
+from groups import views
 
 app_name = 'groups'
 
 urlpatterns = [
+    path('', views.ListGroup.as_view(), name='all'),
+    path('new/', views.CreateGroup.as_view(), name='create'),
+    path('posts/in/<slug:slug>/', views.SingleGroup.as_view(), name='single'),
 ]
